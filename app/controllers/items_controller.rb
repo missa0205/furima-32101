38034650bbd4cca5093804afeab16afb79e703b3
missa0205 <73808394+missa0205@users.noreply.github.com>
2020-12-13 21:48:16 +0900

    商品情報編集機能の指摘修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :call_to_params, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -40,15 +40,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :introduction, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :shipping_day_id, :price, :user_id).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    item = Item.find(params[:id])
-    unless  current_user.id == item.user_id
-      redirect_to action: :index
-    end
-  end
-
   def call_to_params
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless  current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
 end
