0e00410e6350f0d4f2431645ae6b692cf8a9f771
missa0205 <73808394+missa0205@users.noreply.github.com>
2020-12-18 11:06:47 +0900

    商品購入機能の指摘修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,6 +39,13 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user == @item.user
+    if current_user == @item.user
+      redirect_to root_path 
+    end
+
+    if @item.purchase.present?
+      redirect_to root_path
+    end
   end
+
 end
